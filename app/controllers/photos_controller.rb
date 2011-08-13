34965009fc235ee0888cba92b812778b6338996b
Munir Ahmad <munir.ehmad@gmail.com>
2011-08-14 01:04:35 +0500

    first

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,18 @@
+class PhotosController < ApplicationController
+
+	def create
+		@photo = Photo.create( params[:photo] )
+		redirect_to root_url
+	end
+
+
+	def index
+		paths = []
+		Photo.find(:all).each do |photo|
+			paths << photo.picture.url
+		end
+
+		render :json => { :files => paths }.to_json
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fishingbowl::Application.config.secret_token = 'fec55fa895ebbc2daf2e98f01bf66d93f3d69b91db10d61c47bbcc6dc368c948a4e0051839b4a7d09a8e65047d735f9fd8ae7f0f01eae7099a9ce6b06a720b1c'
